d4d706f2bf99e6f2543593719b8d6b0b36ef35d6
Sean Wallace <sean.wallace.australia@gmail.com>
2022-12-18 14:25:55 +1100

    test build, push, deploy

--- src/components/HelloReact.js
+++ src/components/HelloReact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // TODO: Add a comment explaining what a react component is
 function HelloReact() {
-  const text = 'some text';
+  const text = 'text to show that gh-pages does WORK!';
 
   // TODO: Add a comment explaining what JSX is and the significance of the curly braces
   return <p>Hello World! Here is {text}</p>;
