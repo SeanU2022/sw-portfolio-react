4dca9245e57d874d034b337260e031e86f4016bf
Sean Wallace <sean.wallace.australia@gmail.com>
2022-12-18 14:08:04 +1100

    Updates

---
+++ precache-manifest.278571e945c7016bff453c48fa56f888.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "68cdcd8f28e7d7222071",
+    "url": "/sw-portfolio-react/static/js/main.68cdcd8f.chunk.js"
+  },
+  {
+    "revision": "8d636f3623881f0346bd",
+    "url": "/sw-portfolio-react/static/js/1.8d636f36.chunk.js"
+  },
+  {
+    "revision": "c7aefe8f32cadf073518",
+    "url": "/sw-portfolio-react/static/js/runtime~main.c7aefe8f.js"
+  },
+  {
+    "revision": "7ecd604e9d56ac90a591e5addf829a2e",
+    "url": "/sw-portfolio-react/index.html"
+  }
+];
